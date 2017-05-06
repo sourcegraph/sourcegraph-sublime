5ac5c2fd8dbe92ea75df609192a8c1c9faf13420
Stephen Gutekanst <stephen.gutekanst@gmail.com>
2017-05-05 21:06:14 -0700

    all: release v1.0.4

--- sourcegraph.py
+++ sourcegraph.py
@@ -13,7 +13,7 @@ if platform.system() == 'Windows':
     startupinfo.dwFlags |= subprocess.STARTF_USESHOWWINDOW
     startupinfo.wShowWindow = subprocess.SW_HIDE
 
-VERSION = 'v1.0.3'
+VERSION = 'v1.0.4'
 FILENAME_SETTINGS = 'Sourcegraph.sublime-settings'
 
 # gitRemotes returns the names of all git remotes, e.g. ['origin', 'foobar']
@@ -49,61 +49,11 @@ def gitBranch(repoDir):
 	proc = subprocess.Popen(['git', 'rev-parse', '--abbrev-ref', 'HEAD'], stdout=subprocess.PIPE, cwd=repoDir, startupinfo=startupinfo)
 	return proc.stdout.read().decode('utf-8').rstrip()
 
-# removePrefixes removes any of the given prefixes from the input string `s`.
-# Only one prefix is removed.
-def removePrefixes(s, prefixes):
-	for p in prefixes:
-		if s.startswith(p):
-			return s[len(p):]
-	return s
-
-# replaceLastOccurrence returns `s` with the last occurrence of `a` replaced by
-# `b`.
-def replaceLastOccurrence(s, a, b):
-	k = s.rfind(a)
-	if k == -1:
-		return s
-	return s[:k] + b + s[k+1:]
-
-# repoFromRemoteURL returns the repository name from the remote URL. An
-# exception is raised if it cannot be determined. Supported formats are:
-#
-# 	optional("ssh://" OR "git://" OR "https://" OR "https://")
-# 	+ optional("username") + optional(":password") + optional("@")
-# 	+ "github.com"
-# 	+ "/" OR ":"
-# 	+ "<organization>" + "/" + "<username>"
-#
-def repoFromRemoteURL(remoteURL):
-	# Normalize all URL schemes into 'http://' just for parsing purposes. We
-	# don't actually care about the scheme itself.
-	r = removePrefixes(remoteURL, ['ssh://', 'git://', 'https://', 'http://'])
-
-	# Normalize github.com:foo/bar -> github.com/foo/bar -- Note we only do the
-	# last occurrence as it may be included earlier in the case of 'foo:bar@github.com'
-	r = replaceLastOccurrence(r, ':', '/')
-
-	u = urlparse('http://' + r)
-	if not u.netloc.endswith('github.com'): # Note: using endswith because netloc may have 'username:password@' prefix.
-		raise Exception('repository remote is not github.com', remoteURL)
-	return 'github.com' + u.path
-
 def sourcegraphURL(settings):
 	sourcegraphURL = settings.get('SOURCEGRAPH_URL')
 	if not sourcegraphURL.endswith('/'):
 		return sourcegraphURL + '/'
 	return sourcegraphURL
-
-def lineHash(row, col, row2, col2):
-	return 'L' + str(row+1) + ':' + str(col+1) + '-' + str(row2+1) + ':' + str(col2+1)
-
-def branchStr(branch):
-	if branch == 'HEAD':
-		return '' # Detatched head state
-	if branch == 'master':
-		# Assume master is the default branch, for now.
-		return ''
-	return '@' + branch
 
 # repoInfo returns the Sourcegraph repository URI, and the file path relative
 # to the repository root. If the repository URI cannot be determined, an
@@ -119,16 +69,16 @@ def repoInfo(fileName):
 
 		# Determine file path, relative to repository root.
 		fileRel = fileName[len(repoRoot)+1:]
-		repo = repoFromRemoteURL(gitDefaultRemoteURL(repoRoot))
+		remoteURL = gitDefaultRemoteURL(repoRoot)
 		branch = gitBranch(repoRoot)
 	except Exception as e:
 		print("repoInfo:", e)
-	return repo, branch, fileRel
+	return remoteURL, branch, fileRel
 
 class SourcegraphOpenCommand(sublime_plugin.TextCommand):
 	def run(self, edit):
-		repo, branch, fileRel = repoInfo(self.view.file_name())
-		if repo == None:
+		remoteURL, branch, fileRel = repoInfo(self.view.file_name())
+		if remoteURL == None:
 			return
 
 		# For now, we assume the first selection is the most interesting one.
@@ -137,7 +87,17 @@ class SourcegraphOpenCommand(sublime_plugin.TextCommand):
 
 		# Open in browser
 		settings = sublime.load_settings(FILENAME_SETTINGS)
-		url = sourcegraphURL(settings) + repo + branchStr(branch) + '/-/blob/' + fileRel + '?' + urlencode({'utm_source': 'Sublime-' + VERSION}) + '#' + lineHash(row, col, row2, col2)
+		url = sourcegraphURL(settings)+'-/editor?' + urlencode({
+			'remote_url': remoteURL,
+			'branch': branch,
+			'file': fileRel,
+			'editor': 'Sublime',
+			'version': VERSION,
+			'start_row': row,
+			'start_col': col,
+			'end_row': row2,
+			'end_col': col2,
+		})
 		webbrowser.open(url, new=2)
 
 class SourcegraphSearchCommand(sublime_plugin.TextCommand):
@@ -151,5 +111,5 @@ class SourcegraphSearchCommand(sublime_plugin.TextCommand):
 
 		# Search in browser
 		settings = sublime.load_settings(FILENAME_SETTINGS)
-		url = sourcegraphURL(settings) + 'search' + '?' + urlencode({'q': query, 'utm_source': 'Sublime-' + VERSION})
+		url = sourcegraphURL(settings) + '-/editor?' + urlencode({'search': query, 'editor': 'Sublime', 'version': VERSION})
 		webbrowser.open(url, new=2)
